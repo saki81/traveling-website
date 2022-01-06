d24f1e11280bfcee411c41274645b09edefe3ec5
unknown <cike198110@hotmail.com>
2022-01-06 22:00:12 +0100

    add adventure and client section

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// burger-menu 
 const burgerBtn = document.querySelector(".burger-menu");
 const list = document.querySelector(".list");
 
@@ -14,5 +15,56 @@ document.querySelectorAll(".list").forEach(function(n){
 
         e.preventDefault()
     })
-})
+});
+// Scroll page
+const links = document.querySelectorAll(".navigation ul a");
+
+for(const link of links) {
+    link.addEventListener('click', clickHandler);
+}
+
+function clickHandler(e) {
+   e.preventDefault();
+   const href = this.getAttribute("href");
+
+   document.querySelector(href).scrollIntoView({
+       behavior: "smooth"
+   })
+}
+// Show view adventures text
+const camp = document.querySelector('#camp');
+const swimm = document.querySelector('#swimm');
+const mount = document.querySelector('#mount');
+
+const campView = document.querySelector('.camp-view');
+const swimView = document.querySelector('.swim-view');
+const hikingView = document.querySelector('.hiking-view');
+
+camp.addEventListener('click', function() {
+    campView.style.display = "block";
+    swimView.style.display = "none";
+    hikingView.style.display = "none";
+
+    camp.style.opacity = "0.7";
+    swimm.style.opacity = "1";
+    mount.style.opacity = "1";
+});
+swimm.addEventListener('click', function() {
+    campView.style.display = "none";
+    swimView.style.display = "block";
+    hikingView.style.display = "none";
+    
+    camp.style.opacity = "1";
+    swimm.style.opacity = "0.7";
+    mount.style.opacity = "1";
+});
+mount.addEventListener('click', function() {
+    campView.style.display = "none";
+    swimView.style.display = "none";
+    hikingView.style.display = "block";
+
+    camp.style.opacity = "1";
+    swimm.style.opacity = "1";
+    mount.style.opacity = "0.7";
+});
 
